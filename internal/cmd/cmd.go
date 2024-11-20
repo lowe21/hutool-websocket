1c934ed836e9defdd1620dc05841a839296555b3
lowe21 <78665806@qq.com>
2024-11-20 15:56:07 +0800

    增加模拟数据推送

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -2,14 +2,29 @@ package cmd
 
 import (
 	"context"
+	"time"
 
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/ghttp"
 	"github.com/gogf/gf/v2/os/gcmd"
+	"github.com/gogf/gf/v2/util/grand"
+
+	"websocket/internal/pkg/websocket"
 )
 
 var Main = gcmd.Command{
 	Func: func(context.Context, *gcmd.Parser) error {
+		go func() {
+			for {
+				<-time.After(100 * time.Millisecond)
+				data := make([]map[string]any, 0, 10)
+				for i := 0; i < 10; i++ {
+					data = append(data, map[string]any{"x": i, "y": grand.Intn(100)})
+				}
+				websocket.Notice(websocket.Message("data", data))
+			}
+		}()
+
 		server := g.Server()
 		server.Use(ghttp.MiddlewareCORS)
 		server.Run()
